634405b5f869e539d2dd31ccc08ff0d4c62ac6a4
Ekta Singh <ektasingh19.12.2004@gmail.com>
2026-04-25 00:31:41 +0530

    Cleaned server entry point and removed unnecessary comments

--- server/server.js
+++ server/server.js
@@ -2,23 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
-// Load config
 dotenv.config();
 
-// Connect to Database
 connectDB();
 
 const app = express();
-
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use('/api/v1/user', require('./routes/userRoutes'));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes")); // <--- YE HAI MISSING LINE 🟢
 
-// Test Route
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
